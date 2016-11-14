13146243668ccd4a6fbb9953de0ece4231e7435d
Benjamin Noland <benjaminnoland93@gmail.com>
2016-11-14 09:14:27 -0500

    More styling + Street View in info windows

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,31 @@
 
+function makeInfoWindow(event, svData) {
+    var infoWindowDiv = document.createElement("div");
+    
+    if (svData != null) {
+        var panoramaDiv = document.createElement("div");
+        panoramaDiv.style.width = "350px";
+        panoramaDiv.style.height = "200px";
+        infoWindowDiv.appendChild(panoramaDiv);
+        
+        var panorama = new google.maps.StreetViewPanorama(panoramaDiv, {
+            pano: svData.location.pano,
+            pov: {
+                heading: 270,
+                pitch: 0
+            },
+            visible: true
+        });
+    }
+    
+    var infoWindow = new google.maps.InfoWindow({
+        content: infoWindowDiv,
+        position: event.latLng
+    });
+    
+    return infoWindow;
+}
+
 function initMap() {
     var nyc = new google.maps.LatLng(40.73, -73.99);
     var map = new google.maps.Map(document.getElementById("map"), {
@@ -9,9 +36,9 @@ function initMap() {
         mapTypeControl: true,
         mapTypeControlOptions: {
             position: google.maps.ControlPosition.RIGHT_BOTTOM
-        }
+        },
+        streetViewControl: false
     });
-    
     
     // Layer for demo purposes.
     // TODO: Remove.
@@ -25,10 +52,33 @@ function initMap() {
                 iconName: "small_blue"
             }
         }],
+        suppressInfoWindows: true,
         map: map
     });
     
+    var sv = new google.maps.StreetViewService();
+    var infoWindow = null;
+    
     layer.addListener("click", function(event) {
+        if (infoWindow != null) {
+            console.log("Closing...");
+            infoWindow.close();
+        }
+        
+        var request = {
+            location: event.latLng,
+            preference: "nearest"
+        };
+        
+        sv.getPanorama(request, function(data, status) {
+            if (status == "OK")
+                infoWindow = makeInfoWindow(event, data);
+            else
+                infoWindow = makeInfoWindow(event, null);
+            
+            infoWindow.open(map);
+        });
+        
         map.panTo(event.latLng);
     });
 }
